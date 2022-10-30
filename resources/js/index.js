cb4fe5646c2ad1489eeb30bc07ad0485be430f53
Nghiêm Đăng <nghiemkhachaidang25@gmail.com>
2022-10-31 00:28:25 +0700

    add modal

---
+++ resources/js/index.js
@@ -0,0 +1,10 @@
+// open-close MODAL
+
+const signup_btn = document.querySelector(".signup-btn");
+const modal = document.querySelector(".section-modal");
+const btn_close = document.querySelector(".btn-close");
+// open modal
+
+signup_btn.addEventListener("click", () => {
+  console.log("show");
+});
