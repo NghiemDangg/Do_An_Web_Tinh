4b0a68adab43f8c3d87245ee59a486b243f65e31
Nghiêm Đăng <nghiemkhachaidang25@gmail.com>
2022-11-02 17:57:56 +0700

    delete modal page and add modal for home page

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,10 +1,10 @@
 // open-close MODAL
 
-const signup_btn = document.querySelector(".signup-btn");
-const modal = document.querySelector(".section-modal");
-const btn_close = document.querySelector(".btn-close");
-// open modal
+// const signup_btn = document.querySelector(".signup-btn");
+// const modal = document.querySelector(".section-modal");
+// const btn_close = document.querySelector(".btn-close");
+// // open modal
 
-signup_btn.addEventListener("click", () => {
-  console.log("show");
-});
+// signup_btn.addEventListener("click", () => {
+//   console.log("show");
+// });
